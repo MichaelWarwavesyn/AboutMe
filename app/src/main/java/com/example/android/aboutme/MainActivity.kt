ddbe0b5529165ad690e4019754705474ebabf90d
Michael Warwavesyn <MichaelWarwavesyn@gmail.com>
2020-11-27 16:06:15 -0500

    Initial code commit

---
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.android.aboutme
+
+import android.annotation.SuppressLint
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.android.aboutme.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
+    // Instance of MyName data class.
+    private val myName: MyName = MyName("Michael Warwavesyn")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
+
+        //findViewById<Button>(R.id.done_button).setOnClickListener {
+        //  addNickname(it)
+        //
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    @SuppressLint("ServiceCast")
+    private fun addNickname(view: View){
+
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            // Invalidate all binding expressions and request a new rebind to refresh UI
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
